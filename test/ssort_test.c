6ab5f418b19708b4dfdc8bce1b4156e3f01bdbc6
prownd <545751287@qq.com>
2016-07-27 11:45:49 +0800

    add bubble test  in ssort_test.h

--- test/ssort_test.c
+++ test/ssort_test.c
@@ -25,27 +25,39 @@ int compare_persion(const void * pv1,const void* pv2){
 	return name_cmp?name_cmp:s2.age-s1.age;
 }
 
-
+//#define BUBBLE_SORT
+//gcc  ssort_test.c  -DPRINT_FUNCTION_NAME -DBUBBLE_SORT
 int main(){
-	int i;
-		
+		int i;
         int na[]={34,56,17,88,41,69,24,73,81,6};
-	quick_sort(na,sizeof(na)/sizeof(na[0]),sizeof(na[0]),compare_int);
-	for(i=0;i<sizeof(na)/sizeof(na[0]);i++)
-		printf("%d ",na[i]);
-	printf("\n");
+#if defined BUBBLE_SORT
+		bubble_sort(na,sizeof(na)/sizeof(na[0]),sizeof(na[0]),compare_int);
+#elif defined QUICK_SORT
+		quick_sort(na,sizeof(na)/sizeof(na[0]),sizeof(na[0]),compare_int);
+#endif
+		for(i=0;i<sizeof(na)/sizeof(na[0]);i++)
+				printf("%d ",na[i]);
+		printf("\n");
 		
         char * pa[]={"beijing","shanghai","washingtons","seattle"};
-	quick_sort(pa,sizeof(pa)/sizeof(pa[0]),sizeof(pa[0]),compare_string);
-	for(i=0;i<sizeof(pa)/sizeof(pa[0]);i++)
-		printf("%s ",pa[i]);
-	printf("\n");
+#if defined BUBBLE_SORT
+		bubble_sort(pa,sizeof(pa)/sizeof(pa[0]),sizeof(pa[0]),compare_string);
+#elif defined QUICK_SORT
+		quick_sort(pa,sizeof(pa)/sizeof(pa[0]),sizeof(pa[0]),compare_string);
+#endif
+		for(i=0;i<sizeof(pa)/sizeof(pa[0]);i++)
+				printf("%s ",pa[i]);
+		printf("\n");
 
-	struct Persion sa[]={{"jime",22},{"peter",23},{"hanks",20},{"linda",26}};
-	quick_sort(sa,sizeof(sa)/sizeof(sa[0]),sizeof(sa[0]),compare_persion);
-	for(i=0;i<sizeof(sa)/sizeof(sa[0]);i++)
-		printf(" %s,%d ",sa[i].name,sa[i].age);
-	printf("\n");
+		struct Persion sa[]={{"jime",22},{"peter",23},{"hanks",20},{"linda",26}};
+#if defined BUBBLE_SORT
+		bubble_sort(sa,sizeof(sa)/sizeof(sa[0]),sizeof(sa[0]),compare_persion);
+#elif defined QUICK_SORT
+		quick_sort(sa,sizeof(sa)/sizeof(sa[0]),sizeof(sa[0]),compare_persion);
+#endif
+		for(i=0;i<sizeof(sa)/sizeof(sa[0]);i++)
+				printf(" %s,%d ",sa[i].name,sa[i].age);
+		printf("\n");
 		
         return 0;
 }
