d35245b5309c19902fd789249a218e4548d4e833
prownd <545751287@qq.com>
2016-07-27 11:45:22 +0800

    add bubble sort method in ssort.h

--- src/ssort.h
+++ src/ssort.h
@@ -4,6 +4,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
+//bubble sort
+void bubble_sort(void* data,int len,int size,int(*compare)(const void*,const void*));
+
+//quick sort partition
 void quick_sort_partition(void * base,size_t left,size_t right,size_t size,int(* compare)(const void *,const void *));
+
+//quick sort
 void quick_sort(void* base,size_t nmemb,size_t size ,int(*compare)(const void *,const void*));
+
 #endif //_SSORT_H_
