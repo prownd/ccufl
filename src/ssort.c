12e64bce89b2aa25dc00c30a62b1a7ee02c876e7
hanjinpeng <545751287@qq.com>
2016-07-27 17:55:52 +0800

    Update ssort.c

--- src/ssort.c
+++ src/ssort.c
@@ -7,9 +7,9 @@
 
 void bubble_sort(void* data,int len,int size,int(*compare)(const void*,const void*)){
 #ifdef PRINT_FUNCTION_NAME
-    printf("%s\n",__FUNCTION__);
+	printf("%s\n",__FUNCTION__);
 #endif
-    int i,j;
+	int i,j;
 	void* temp=malloc(size);
 	for(i=0;i<len;i++)
 	{	
@@ -27,25 +27,25 @@ void bubble_sort(void* data,int len,int size,int(*compare)(const void*,const voi
 		if(ordered)
 			break;
 	}
-    free(temp);
+	free(temp);
 }
 
 void insert_sort(void* data,int len,int size, int(*compare)(const void*,const void*)){
 #ifdef PRINT_FUNCTION_NAME
-    printf("%s\n",__FUNCTION__);
+    	printf("%s\n",__FUNCTION__);
 #endif
-    int i,j;
-    void *temp=malloc(size);
+	int i,j;
+	void *temp=malloc(size);
 	for(i=1;i<len;i++)
 	{
 		memcpy(temp,data+i*size,size);
 		for(j=i;j>0&&(compare(temp,data+(j-1)*size)<0);j--)
 		{
-        		memcpy(data+j*size,data+(j-1)*size,size);
+			memcpy(data+j*size,data+(j-1)*size,size);
 		}
 		memcpy(data+j*size,temp,size);
 	}
-    free(temp);
+    	free(temp);
 }
 
 void quick_sort_partition(void * base,size_t left,size_t right,size_t size,int(* compare)(const void *,const void *)){
@@ -80,7 +80,7 @@ void quick_sort_partition(void * base,size_t left,size_t right,size_t size,int(*
 
 void quick_sort(void* base,size_t nmemb,size_t size ,int(*compare)(const void *,const void*)){
 #ifdef PRINT_FUNCTION_NAME
-    printf("%s\n",__FUNCTION__);
+	printf("%s\n",__FUNCTION__);
 #endif
 	quick_sort_partition(base,0,nmemb-1,size,compare);
 }
